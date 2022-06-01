bc90cfa743648da511bf93a6829878d324c0c6df
AHZ456 <83831043+AHZ456@users.noreply.github.com>
2022-06-01 16:14:54 +0100

    Add files via upload

---
+++ main/I2C.cpp
@@ -0,0 +1,271 @@
+#include "I2C.hpp"
+
+
+
+
+
+
+
+
+void SET_REG(uint8_t ADDR, uint8_t REG, uint8_t *DATA, uint8_t NUM)
+{
+
+  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
+  i2c_master_start(cmd);
+  i2c_master_write_byte(cmd, ((ADDR << 1) | WR), ACK);
+  i2c_master_write_byte(cmd, REG, ACK);
+  i2c_master_write(cmd, DATA, NUM, ACK);
+  i2c_master_stop(cmd);
+  esp_err_t err = i2c_master_cmd_begin(i2c_master_port, cmd, WAIT);
+  CHECK(err);
+  i2c_cmd_link_delete(cmd);
+  CHECK(err);
+}
+
+void SET_READ(uint8_t ADDR, uint8_t REG)
+{
+  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+  i2c_master_start(cmd);
+  i2c_master_write_byte(cmd, ((ADDR << 1) | WR), ACK);
+  i2c_master_write_byte(cmd, REG, ACK);
+  i2c_master_stop(cmd);
+  esp_err_t err = i2c_master_cmd_begin(i2c_master_port, cmd, WAIT);
+  CHECK(err);
+  i2c_cmd_link_delete(cmd);
+  CHECK(err);
+}
+void SET_READ_CUR()
+{
+#if SHUNT_CKT
+  SET_READ(addrINA, CUR_REG);
+#elif LPA_CKT || HALL_CKT
+  SET_READ(addrADS, ADS_CUR);
+#endif
+}
+
+
+
+void INIT()
+{
+
+  // measure offset
+
+  i2c_config_t conf; 
+
+      conf.mode = I2C_MODE_MASTER;
+      conf.sda_io_num = GPIO_NUM_25;
+      conf.sda_pullup_en = GPIO_PULLUP_DISABLE;
+      conf.scl_io_num = GPIO_NUM_27;
+      conf.scl_pullup_en = GPIO_PULLUP_DISABLE;
+      conf.master.clk_speed = 390000;
+  
+
+  esp_err_t err = i2c_param_config(i2c_master_port, &conf);
+  // sus
+  CHECK(err);
+  err = i2c_driver_install(i2c_master_port, conf.mode, 0, 0, 0);
+  CHECK(err);
+
+#if SHUNT_CKT
+  uint8_t DATA1[2] = {0x21, 0x9d};
+  //40mV PGA
+  uint8_t DATA2[2] = {0x57, 0x61};
+  //for 10A  (12 bits)
+
+  SET_REG(addrINA, CONFIG_REG, DATA1, 2);
+  SET_REG(addrINA, CAL_REG, DATA2, 2);
+#elif LAP_CKT
+ uint8_t DATA1[2] = {0x86, 0xe3};
+   // RECHECK
+  //AINp= AIN0
+  //AINn=AIN1
+  SET_REG(addrADS, ADS_CONFIG, DATA1, 2);
+#elif HALL_CKT
+  uint8_t DATA1[2] = {0xb6, 0xe3};
+  //AINp=AIN2
+  //AINn=AIN3
+  SET_REG(addrADS, ADS_CONFIG, DATA1, 2);
+#endif  
+}
+
+
+
+
+
+
+int16_t I2C_READ()
+{
+#if SHUNT_CKT
+  uint8_t ADDR = addrINA;
+#elif LPA_CKT || HALL_CKT
+  uint8_t ADDR = addrADS;
+#endif
+
+  uint8_t resultA[2] = {0};
+  uint16_t result = 0;
+  i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+  esp_err_t err = i2c_master_start(cmd);
+  i2c_master_write_byte(cmd, ((ADDR << 1) | RD), ACK);
+  // i2c_master_read(cmd,resultA,2,I2C_MASTER_ACK);
+  i2c_master_read_byte(cmd, &resultA[0], I2C_MASTER_ACK);
+  i2c_master_read_byte(cmd, &resultA[1], I2C_MASTER_LAST_NACK);
+  i2c_master_stop(cmd);
+  err = i2c_master_cmd_begin(i2c_master_port, cmd, WAIT);
+  CHECK(err);
+  i2c_cmd_link_delete(cmd);
+  CHECK(err);
+/*#if SHUNT_CKT 
+  result = (((uint16_t)resultA[0] << 8) | ((uint16_t)resultA[1]))-27;
+#elif LPA_CKT
+  result = (((uint16_t)resultA[0] << 8) | ((uint16_t)resultA[1]));*/
+#if SHUNT_CKT || LPA_CKT
+result = (((uint16_t)resultA[0] << 8) | ((uint16_t)resultA[1]));
+#elif HALL_CKT
+  result = (((uint16_t)resultA[0] << 8) | ((uint16_t)resultA[1])) - MAGOFFSET;
+#endif
+  return result;
+}
+
+
+
+
+void delay(uint64_t wait)
+{
+  uint64_t microseconds = (uint64_t)esp_timer_get_time();
+  while ((uint64_t)esp_timer_get_time() >= 0xffffffffffffffff - wait)
+    ;
+  while ((uint64_t)esp_timer_get_time() - microseconds < wait)
+    ;
+}
+
+current EFF_CUR()
+{
+
+  int16_t var = I2C_READ();
+
+  current HL = {0,0,0};
+  if (((var - ID) < 0))
+  {
+    HL.i = var;
+    HL.ip1 = -powl((double)(-(var - ID)) / DIVC, pc1);
+    HL.ip2 = -powl((double)(-(var - ID)) / DIVC, pc2);
+  }
+  else
+  {
+    HL.i = var;
+    HL.ip1 = powl((double)((var - ID)) / DIVC, pc1);
+    HL.ip2 = powl((double)((var - ID)) / DIVC, pc2);
+  }
+
+  return HL;
+}
+
+
+
+MEAS VBAT(adc1_channel_t channel, adc1_channel_t channel2,
+          esp_adc_cal_characteristics_t *adc_chars, int NO_OF_SAMPLES)
+{
+  uint32_t adc_reading = 0;
+  uint32_t adc_reading2 = 0;
+  double voff = 0.0;
+  // Multisampling
+  for (int i = 0; i < NO_OF_SAMPLES; i++)
+  {
+    adc_reading += adc1_get_raw((adc1_channel_t)channel);
+    adc_reading2 += adc1_get_raw((adc1_channel_t)channel2);
+  }
+  adc_reading /= NO_OF_SAMPLES;
+  adc_reading2 /= NO_OF_SAMPLES;
+  // Convert adc_reading to voltage in mV
+  double voltage = (double)esp_adc_cal_raw_to_voltage(adc_reading, adc_chars) / 1000;   // GPIO34
+  double voltage2 = (double)esp_adc_cal_raw_to_voltage(adc_reading2, adc_chars) / 1000; // GPIO35
+  double cf = calv1 * (double)((260 + 10000) / 260);                                              // GPIO34 (24 V)
+  double cf2 = calv2 * (double)((470 + 10000) / 470);                                             // GPIO35 (12 V)
+#if SHUNT_CKT
+  SET_READ(addrINA, VS_REG);
+  voff = (double)I2C_READ() / VLSB;
+
+    SET_READ_CUR();
+#endif
+  MEAS batteries = {(voltage)*cf - (voltage2)*cf2, (voltage2)*cf2 - voff, voff};
+  return batteries;
+}
+
+void SOC_SET(MEAS vini, uint8_t num)
+{
+  if (num == 0)
+  {
+    SOC1 = (vini.v1 - bcoef) / acoef;
+  }
+  else if (num == 1)
+  {
+    SOC2 = (vini.v1 - dcoef) / ccoef;
+  }
+  else if (num == 2)
+  {
+    SOC1 = (vini.v1 - bcoef) / acoef;
+    SOC2 = (vini.v1 - dcoef) / ccoef;
+  }
+  // linear approximation
+}
+
+void FEEDBACK()
+{
+  int64_t T3 = 0, T4 = 0;
+  double Current=0.0;
+  static uint8_t VGS = 0;
+  //2.89V => 4V amplified
+   T3 = (uint64_t)esp_timer_get_time();
+
+  for (int i = 0; i < 500; i++)
+  {
+    
+     Current = -1*((double)(fb.i - ID) / DIVC);
+     
+     
+   
+  if(update ==1){
+    if (Current > LMT && VGS != 0)
+    {
+      VGS--;
+      
+    }
+    else if (Current < LMT && VGS != LMTDAC)
+    {
+      VGS++;
+    }
+
+    dac_output_voltage(DAC_CHANNEL_2, VGS);
+    update=0;
+   
+    }
+
+  }
+
+  
+    /*T4 = (uint64_t)esp_timer_get_time();
+  printf("RESPONSE: %lf\t CURRENT :%lf A\n",(double)(T4 - T3) / DIVT,Current);*/
+}
+
+
+void SEND(MEAS value,float Charge1){
+
+
+
+
+
+    // Create Feed
+    
+    feed.created_at = "";   // setting it to "" uses current datetime, setting to datetime_iso8061 stores the data with this specific datetime
+    feed.fields.push_back({1, (float)(value.v1)}); //< field1 = 11.11f
+    feed.fields.push_back({3, (Charge1)}); //< field3 = 33.33f
+
+
+    // Post Feed  
+   ThingSpeak client(THINGSPEAK_CHANNEL_ID, THINGSPEAK_API_KEY);
+    client.post(feed);
+
+
+
+}
---
+++ main/I2C.hpp
@@ -0,0 +1,149 @@
+#ifndef INA_I2C_H_
+#define INA_I2C_H_
+
+#include "driver/i2c.h"
+#include <math.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+#include <driver/dac.h>
+
+//Thingspeak libraries and defines
+
+
+
+#include <iostream>
+#include "esp_tls.h"
+#include "esp_http_client.h"
+#include "ThingSpeak.h"
+
+#define SSID "Galaxy A10"
+#define PASSWORD "bela3foumek"
+#define THINGSPEAK_CHANNEL_ID "1694476"
+#define THINGSPEAK_API_KEY "8PECLZUDD1882KOP"   // Write api key
+
+//
+#define SHUNT_CKT 1
+#define LPA_CKT 0
+#define HALL_CKT 0
+/*Current method:
+SHUNT_CKT-Shunt based sensing
+LPA_CKT -Current transducer
+HALL_CKT-Hall effect sensor
+ONLY one should be active (1) at a time
+*/
+
+#define ACK 0x1
+#define WR 0x0
+#define RD 0x1
+#define CONFIG_REG 0x00
+#define CAL_REG 0x05
+#define CUR_REG 0x04
+#define VS_REG 0x01
+/// ADS registers
+#define ADS_CONFIG 0x01
+#define ADS_CUR 0x00
+
+#define i2c_master_port I2C_NUM_0
+#define WAIT 0xffff
+#define addrINA 0x40
+#define addrADS 0x48
+#define LTHRESHOLD1 10
+#define UTHRESHOLD1 13
+#define LTHRESHOLD2 10
+#define UTHRESHOLD2 13
+#define DEFAULT_VREF 1086 // Use adc2_vref_to_gpio() to obtain a better estimate
+// CHECK AGAIN WITH THE NEW CHIP
+#define calv1 1
+#define calv2 1
+#define VLSB 100000
+#define DIVT 1000000
+#define LPAPGA 1.024
+#define HALLPGA 1.024
+#define TURN 1000
+// turn ration of the CT
+#define SENS 0.02
+// sensitivity of the hall sensor
+#define LMT 0.5
+//current of the active load
+#define LMTDAC 243
+//maximum output for the DAC
+//243 key value for 6 A
+
+#define TSTDAC 167
+//we try to run 1 A through the load to see if the battery is charged
+
+#define CUR_THSH 0.05
+//cut off current ( for battery discharge tests)
+
+
+
+#define pc1 1
+#define pc2 1
+
+#define NQ1 1
+// NQ1=charge efficiency/nominal capacity of battery1
+#define NQ2 1
+// NQ2=charge efficiency/nominal capacity of battery2
+#define ID 0
+// Self-discharge current in CUR_LSBs
+// CUR_LSB=50µA
+#define acoef 1
+#define bcoef 1
+#define ccoef 1
+#define dcoef 1
+// the coefficients of the linear approximation
+
+#define CHECK(err) ESP_ERROR_CHECK(err);
+
+struct
+{
+  int16_t i;
+  long double ip1;
+  long double ip2;
+
+} typedef current;
+
+struct
+{
+  double v1;
+  double v2;
+  double vo;
+} typedef MEAS;
+
+extern float SOC1;
+extern float SOC2;
+// Configure ADC
+extern esp_adc_cal_characteristics_t *adc_chars;
+extern const adc1_channel_t channel; // GPIO34 if ADC1, GPIO14 if ADC2
+extern const adc1_channel_t channel2; // GPIO35 CHANNEL7
+extern const adc_bits_width_t width;
+extern const adc_atten_t atten;
+extern const adc_unit_t unit;
+extern const long double DIVC;
+extern const long double DIVCP1;
+extern const long double DIVCP2;
+extern const long double MAGOFFSET;
+extern const long double Cn1;
+extern const long double Cn2;
+extern current fb;
+extern uint8_t update;
+extern ThingSpeak::Feed feed;
+extern uint8_t SYS_ON;
+
+
+void SET_REG(uint8_t ADDR, uint8_t REG, uint8_t *DATA, uint8_t NUM);
+void SET_READ(uint8_t ADDR, uint8_t REG);
+int16_t I2C_READ();
+void INIT();
+current EFF_CUR();
+void delay(uint64_t wait);
+MEAS VBAT(adc1_channel_t channel, adc1_channel_t channel2,
+          esp_adc_cal_characteristics_t *adc_chars, int NO_OF_SAMPLES);
+void SOC_SET(MEAS vini, uint8_t num);
+void FEEDBACK();
+void SET_READ_CUR();
+void SEND(MEAS value,float Charge1);
+#endif
